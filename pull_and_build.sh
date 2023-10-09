3a7e02f8a41300a35c2607c1ee2acc6f4e93454b
sekulicd <sekula87@gmail.com>
2023-10-09 11:00:26 +0200

    fix

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -16,7 +16,7 @@ BRANCH=$3
 if [[ ! -d "$REPO" ]]; then
     # If not, create it, cd into it, and clone the repo
     mkdir "$REPO" && cd "$REPO"
-    git clone "https://github.com/$USER/$REPO.git" --branch "$BRANCH" --single-branch .
+    git clone "https://github.com/$USER/$REPO.git" --branch "$BRANCH" --single-branch "$REPO"
 else
     # If it does, cd into it
     cd "$REPO"
